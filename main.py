d89a4e0c3fc2dda759a551ab5723882d9574ed97
Arjun Patel <arjun@aspatel.com>
2019-03-07 18:48:51 +0000

    Complete first stage of data parsing code

--- main.py
+++ main.py
@@ -1,13 +1,4 @@
 '''
-This query will return the note data of all cards that you have learned
-in the Kanji deck and put the result into 'learned_vocab.txt':
-
-select n.flds
-from cards c 
-join notes n on c.nid = n.id
-where c.type = 2
-and c.did = 1543218842369
-
 Get all of the sort fields (IDs) of the vocab from the Japanese deck which 
 are inside of new cards and put it into 'unlearned_vocab.txt':
 
@@ -22,7 +13,8 @@ order by n.sfld
 Make sure to do both of the above steps before running the code in 
 this file!
 '''
-import DatabaseHelper
+from databasehelper import DatabaseHelper
+from textparser import TextParser
 
 def extract_cards_to_study_main():
     '''
@@ -80,48 +72,25 @@ def extract_cards_to_study_main():
 
     print(output)
 
-def get_vocab_main():
+def get_learned_rtk_kanji():
     '''
-    Gets all of the learned Kanji from the results of the query:
-    select n.flds
-    from cards c 
-    join notes n on c.nid = n.id
-    where c.type = 2
+    This method will return all of the words that you have learned from the
+    RTK kanji deck as a list, using the below query.
     '''
-    vocab_lst = parse_vocab()
-    vocab_lst = get_usable_vocab(vocab_lst)
-    with open("learned_vocab_parsed.txt","w+") as f:
-        for vocab in vocab_lst:
-            f.write(vocab)
-            f.write("\n")
+    query = "select n.flds "
+    query += "from cards c "
+    query += "join notes n on c.nid = n.id "
+    query += "where c.type = 2 "
+    query += "and c.did = 1543218842369"
+    vocab = DatabaseHelper.execute(__get_database_filepath(), query)
+    return TextParser.kanji_deck_only_main_vocab_word(vocab)
 
-def parse_vocab():
-    lst = []
-    with open("learned_vocab.txt", 'r', encoding='utf-8') as f:
-        for line in f.readlines():
-            lst.append(line.split("")[4])
-    return lst
-
-def get_usable_vocab(lst):
-    vlst = []
-    for v in lst:
-        vlst.append(validate_vocab(v))
-    return vlst
-
-def validate_vocab(v):
-    if ", " in v:
-        v = v.split(", ")[0]
-    if "<div>" in v:
-        v = v[5:]
-    return v
-
-def get_database_filepath():
+def __get_database_filepath():
     with open("db-location.txt", 'r') as f:
         return f.readlines()[0]
 
 if __name__ == '__main__':
-    #get_vocab_main()
+    #get_learned_rtk_kanji()
     #extract_cards_to_study_main()
-    print(get_database_filepath())
-    db = DatabaseHelper()
-    db.test()
+    for vocab in get_learned_rtk_kanji():
+        print(vocab)
