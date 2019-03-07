6db4f50d050b1e4493b22271fe9f2e5c833b1c45
Arjun Patel <arjun492.ap@gmail.com>
2019-03-07 17:08:25 +0000

    Add python class for database methods

---
+++ DatabaseHelper.py
@@ -0,0 +1,10 @@
+import sqlite3
+
+class DatabaseHelper(object):
+	"""
+	A class containing methods that can be used to safely interface with
+	a database
+	"""
+	@staticmethod
+	def placeholder():
+		pass
