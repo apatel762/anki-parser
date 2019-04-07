259dc19f78e9a057f700ecd8716601da89578984
Arjun Patel <arjun@aspatel.com>
2019-04-07 12:33:08 +0100

    Reword comments to be <= 78 characters

--- main.py
+++ main.py
@@ -42,11 +42,11 @@ def __update_database_unsuspend_doable_cards(final_id_lst):
     print(f"The following sentence cards were unsuspended:\n{final_id_lst}")
 
 def __get_unstudied_sentence_card_ids():
-    # Get the IDs of the sentence cards that I haven't yet studied (susp. cards)
+    # Get IDs of sentence cards that I haven't yet studied (i.e. susp. cards)
     query = "select n.sfld "
     query += "from cards c "
     query += "join notes n on c.nid = n.id "
-    # The below will restrict the selection of suspended cards to 'type = new'
+    # Commented code restricts selection of suspended cards to 'type = new'
     #query += "where c.type in 0 "
     #query += "and c.queue = -1 "
     query += "where c.queue = -1 "
